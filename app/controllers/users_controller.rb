d192a6e5fc87b8d3b0d4436e6b0b010595e437da
kishan_codder <kishan.sutex.kp@gmail.com>
2020-02-07 14:27:08 +0530

    Set follow message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,7 @@ class UsersController < ApplicationController
 
   def follow_user
     follow = UserFollow.new(user_id: params[:id], following_user_id: current_user.id)
-    if params[:id].to_i == current_user.id
-      flash[:notice] = "Can not follow yourself."
-    elsif follow.save
+    if follow.save
       flash[:notice] = "Follow Successfully"
     else
       flash[:notice] = "Already following"
